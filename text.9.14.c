02eb0eafb2e7093f7a4c0e2d8599695d897bc43b
wuzizhongqiu <113303808+wuzizhongqiu@users.noreply.github.com>
2022-09-14 21:39:10 +0800

    Add files via upload

---
+++ text.9.14.c
@@ -0,0 +1,65 @@
+
+#include<stdio.h>
+int add(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
+}
+int main()
+{
+	int a = 19;
+	int b = 20;
+	int sum = add(a, b);
+	printf("%d\n",sum);
+	//int arr[10] = { 0 };
+	//arr[4];
+	//printf("%d\n", (arr)); 
+	return 0;
+}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = 0;
+//	max=(a > b ? a : b);
+//	printf("%d\n", max);
+//	return 0;
+//}
+//int mai
+// n()
+//{
+//	int a = (int)3.14;
+//return 0;
+//}
+//int mai
+//n()
+//{
+//	int a = 10;
+//	int b = a++;
+//	printf("a = %d\n b = %d\n", a, b);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = ~a;
+//	printf("%d\n", b);
+//
+//	return 0;
+//}
+//#include<string.h>
+
+//int main()
+//{
+//	int a = 10;
+//	int arr[] = { 1,2,3,4,5,6 };
+//	printf("%d\n", sizeof(a));
+//	printf("%d\n",sizeof(int));
+//	printf("%d\n", sizeof a);
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr) / sizeof(arr[0]));
+//
+//	return 0;
+//}
