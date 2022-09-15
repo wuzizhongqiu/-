4b8dc2f970a013fd8d40ac4bd449c35774a9c87b
wuzizhongqiu <113303808+wuzizhongqiu@users.noreply.github.com>
2022-09-15 21:46:59 +0800

    Add files via upload

---
+++ text.9.15.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+int main()
+{
+	extern int ggg;
+	printf("ggg=%d\n", ggg);
+	return 0;
+}
+//void testz()
+//{
+//	int a = 1;
+//	a++;
+//	printf("a = %d\n", a);	
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i = 5);
+//	test();
+//	i++;
+//	return 0;
+//}
