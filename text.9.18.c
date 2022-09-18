d0dd8cae1c755d4e01886147c3465fca4f62ce93
wuzizhongqiu <113303808+wuzizhongqiu@users.noreply.github.com>
2022-09-18 14:58:51 +0800

    Add files via upload

---
+++ text.9.18.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+int main()
+{
+	int a = 1;
+	while (a < 100)
+	{
+		printf("%d ", a);
+		a += 2;
+	}
+	return 0;
+}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 100)
+//	{
+//		if (i % 2 == 1)
+//			printf("%d", i);
+//			i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int num = 4;
+//	if (5 == num)
+//	//if (num = 5)
+//	{
+//		printf("haha\n");
+//	}
+//	return 0;
+//}
+////int main()
+//{
+//	int a = 10;
+//	if (a < 18)
+//	{
+//		printf("未成年\n");
+//		printf("jjj\n");
+//	}
+//	return 0;
+//}
+//	else if (a >= 18 && a < 28)
+//		printf("青年");
+//	else if (a > 28 && a < 60)
+//		printf("中年");
+//	else if (a >= 60 && a < 90)
+//		printf("老年");
+//	else
+//		printf("暮年");
+//	//else
+//	//	printf("成年");
+//	return 0;
+//}
+////int main()
+////{
+////	int arr[10] = { 0 };
+////	int i = 0;
+////	for (i = 0; i < 10; i++)
+////	{
+//		scanf_s("%d", &arr[i]);
+//	}
+//	int sum = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		sum += arr[i];
+//	}
+//	printf("%d\n", sum / 10);
+//	return 0;
+//}
