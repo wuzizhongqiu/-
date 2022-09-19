4e818dc4fcabad5984c63336c25ccc168036560c
wuzizhongqiu <113303808+wuzizhongqiu@users.noreply.github.com>
2022-09-19 21:59:01 +0800

    Add files via upload

---
+++ text.9.19.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		putchar(ch);
+	}
+	return 0;
+}
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int day;
+//	scanf_s("%d", &day);
+//	switch(day)
+//	{
+//	case 1:
+//		printf("星期1\n");
+//		break;
+//	case 2:
+//		printf("星期2\n");
+//		break;
+//	case 3:
+//		printf("星期3\n");
+//		break;
+//	case 4:
+//		printf("星期4\n");
+//		break;
+//	case 5:
+//		printf("星期5\n");
+//		break;
+//	case 6:
+//		printf("星期6\n");
+//		break;
+//
+//
+//	}
+//	return 0;
+//}
