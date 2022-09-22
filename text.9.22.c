3bd4b1b503ca8b05b448b9fb60ff54df40505cf1
wuzizhongqiu <113303808+wuzizhongqiu@users.noreply.github.com>
2022-09-22 22:05:15 +0800

    Add files via upload

---
+++ text.9.22.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int ret = 1;
+	int n = 0;
+	int sum = 0;
+	for (n = 1; n <= 3; n++)
+	{
+		ret = ret * n;
+		sum = sum + ret;
+	}
+	printf("sum=%d\n", sum);
+	return 0;
+}
